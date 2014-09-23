e8671fefc059b136e381fc34331425531d25a201
Dave Arneson <urvaius@gmail.com>
2014-09-23 16:43:05 -0700

    addedd more on tutoral 2 box2d block bunny 13:27

--- core/src/com/arne5/blockbunny/states/Play.java
+++ core/src/com/arne5/blockbunny/states/Play.java
@@ -1,9 +1,11 @@
 package com.arne5.blockbunny.states;
 
 import com.arne5.blockbunny.handlers.GameStateManager;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
 
 /**
  * Created by darneson on 9/22/2014.
@@ -12,11 +14,39 @@ public class Play extends GameState
 	{
 
 		private World world;
+		private Box2DDebugRenderer b2dr;
 		public Play(GameStateManager gsm)
 			{
 				super(gsm);
 				//using box2d
 				world = new World(new Vector2(0,-9.81f),true);
+				b2dr = new Box2DDebugRenderer();
+
+				//create platform
+				BodyDef bdef = new BodyDef();
+				bdef.position.set(160,120);
+				bdef.type = BodyDef.BodyType.StaticBody;
+				Body body = world.createBody(bdef);
+				//static body - dont' move, unaffected by forces = ground
+
+				// kinematic body = dont' get affected by world forces but can change their velocities = platform
+
+				//dynamic body alwasy get affecdted by forces= player
+				PolygonShape shape = new PolygonShape();
+				shape.setAsBox(50,5);
+				FixtureDef fdef = new FixtureDef();
+				fdef.shape = shape;
+				body.createFixture(fdef);
+
+				// create falling box
+				bdef.position.set(160,200);
+				bdef.type = BodyDef.BodyType.DynamicBody;
+				body = world.createBody(bdef);
+				shape.setAsBox(5, 5);
+				fdef.shape = shape;
+				//make box bouncy
+				fdef.restitution= 1f;
+				body.createFixture(fdef);
 
 			}
 		public void handleInput()
@@ -25,11 +55,18 @@ public class Play extends GameState
 			}
 		public void update(float dt)
 			{
+				//for box 2d
+				world.step(dt,6,2);
+
 
 			}
 		public void render()
 			{
 
+				//clear screen
+				Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+				//draw box2dworld
+				b2dr.render(world,cam.combined);
 
 			}
 		public void dispose()
